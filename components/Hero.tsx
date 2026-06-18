e44974502bf304e38dbf999add5a09099afc2fa1
Proveen Kumar Yadav <140779271+proveenyadav@users.noreply.github.com>
2026-06-18 14:38:00 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import FadeUp from "@/components/FadeUp";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
